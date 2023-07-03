56a14ccb7f2909ea6d981b049a4f24434709da7e
Luis Stasi <stasi93@libero.it>
2023-07-03 13:22:34 +0200

    nav sticky

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,6 +1,6 @@
-<nav class="navbar bg-body-tertiary  navbar-expand-lg container mt-3">
+<nav class="navbar bg-body-tertiary  navbar-expand-lg container mt-3 sticky-md-top">
     <div class="container-fluid">
-        <a class="navbar-brand fw-bold  px-3" href="#header"><img src="./media/logo_enjoy36-removebg-preview.png" alt="img_logo" width="120rem;"></a>
+        <a class="navbar-brand fw-bold  px-3" href="#header"><img src="./media/logo2.png" alt="img_logo" width="120rem;"></a>
         <a href="#contacts" class="btn btn-outline-danger rounded-pill bg-tertiary text-center fw-bold mx-5 px-3">Contattaci</a>
     </div>
 </nav>
